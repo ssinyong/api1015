87fa96ffc6a5b07886e4e24a606d59ac7a575ecb
ssinyong <bsy2889@gmail.com>
2024-10-15 12:44:12 +0900

    2교시 중간

--- src/main/java/org/zerock/api1015/controller/advice/SampleControllerAdvice.java
+++ src/main/java/org/zerock/api1015/controller/advice/SampleControllerAdvice.java
@@ -1,19 +1,33 @@
 package org.zerock.api1015.controller.advice;
 
+import lombok.extern.log4j.Log4j2;
 import org.springframework.http.ResponseEntity;
+import org.springframework.validation.BindingResult;
 import org.springframework.web.bind.MethodArgumentNotValidException;
 import org.springframework.web.bind.annotation.ExceptionHandler;
 import org.springframework.web.bind.annotation.RestControllerAdvice;
 
+import java.util.HashMap;
 import java.util.Map;
 
 @RestControllerAdvice
+@Log4j2
 public class SampleControllerAdvice {
 
     @ExceptionHandler(MethodArgumentNotValidException.class)
-    public ResponseEntity<Map<String, String>> handle(MethodArgumentNotValidException ex){
+    public ResponseEntity<java.util.Map<String,Object>> handle(MethodArgumentNotValidException ex){
 
-        Map<String, String> map = Map.of("error", "Not Valid");
+        BindingResult bindingResult = ex.getBindingResult();
+
+        Map<String,Object> map = new HashMap<>();
+
+        ex.getFieldErrors().forEach(fieldError -> {
+            log.error("-------------");
+            String key = fieldError.getField();
+
+            Object value = fieldError.getDefaultMessage();
+            map.put(key,value);
+        });
 
         return ResponseEntity.status(400).body(map);
     }
--- src/main/java/org/zerock/api1015/dto/SampleDTO.java
+++ src/main/java/org/zerock/api1015/dto/SampleDTO.java
@@ -1,11 +1,15 @@
 package org.zerock.api1015.dto;
 
+import jakarta.validation.constraints.Min;
 import jakarta.validation.constraints.NotNull;
 import lombok.Data;
 
 @Data
 public class SampleDTO {
 
-    @NotNull
+    @NotNull(message = "name cannot be null")
     private String name;
+
+    @Min(value = 10, message = "at least 10")
+    private int size;
 }
